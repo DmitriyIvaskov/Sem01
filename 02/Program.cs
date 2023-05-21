d87f0fed027f29e6002a575aa8ab95ea3f215454
DmitriyIvaskov <citrys2008@gmail.com>
2023-05-21 16:16:06 +0700

    Созадали код для проверки является ли первое число квадратом 2го числа

---
+++ 02/Program.cs
@@ -0,0 +1,19 @@
+﻿// на вход два числа, является ли первое квадртаом 2го?
+// a = 25 b = 5 -> да
+// a = 2 b = 10 -> нет
+// a = 9 b = -3 -> да
+// a = -3 b = 9 -> нет
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondtNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondtNumber * secondtNumber)
+{
+    Console.WriteLine("Число " + firstNumber + " является квадратом числа " + secondtNumber);
+}
+else 
+{
+    Console.WriteLine("Число " + firstNumber + " НЕ является квадратом числа " + secondtNumber);
+}
