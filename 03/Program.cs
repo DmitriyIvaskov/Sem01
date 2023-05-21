5ccd328f26dec23fa39a0017c656c010e8bba9f1
DmitriyIvaskov <citrys2008@gmail.com>
2023-05-21 16:56:42 +0700

    Код для вывода целых чисел 03

---
+++ 03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа которая принимает целое число N а на выходе показывает все целые в промежутке -N до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = N * (-1);
+
+while (negativeN <= N)
+{
+    Console.WriteLine(negativeN);
+    negativeN = negativeN + 1; // или negativeN ++
+}
