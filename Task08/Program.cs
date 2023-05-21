77b614496b4279090944eb9dc8d19e51dc700ded
DmitriyIvaskov <citrys2008@gmail.com>
2023-05-21 17:56:18 +0700

    Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N

---
+++ Task08/Program.cs
@@ -0,0 +1,20 @@
+﻿int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (i <= number)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("нет чётных чисел");
+}
